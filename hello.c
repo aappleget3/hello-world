c8c505897c22a92975d8cb20355b47dade632a20
aappleget3 <42563996+aappleget3@users.noreply.github.com>
2018-08-23 08:18:00 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,16 @@
+//**
+ * Author: AJ Appleget
+ * Date: 2016/11/02
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("AJ Appleget Computer Science\n");
+
+  return 0;
+}
